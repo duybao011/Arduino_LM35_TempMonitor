29d941b1742a1b74dc7789db6745554e83992860
khoinguyen0110 <scnguyen163@gmail.com>
2026-04-13 12:20:27 +0700

    Update LM35_TempReader.ino

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -6,21 +6,7 @@ void setup() {
 }
 
 void loop() {
-  // Kênh A0
-  adcValues[0] = analogRead(A0);
-  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
-  
-  // Kênh A1
-  adcValues[1] = analogRead(A1);
-  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  
-  // Kênh A2 (Vừa mới thêm)
-  adcValues[2] = analogRead(A2);
-  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
-  
-  // Xuất ra Serial
 // Sửa lại thành bản cũ để tạo mâu thuẫn
 sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
 Serial.print(chuoi);
-  delay(100);
 }
