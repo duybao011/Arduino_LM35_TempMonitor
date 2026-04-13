907fcd09d630d8e724e557ae0b02074d3d4eeab9
Lộc <scnguyen@gmail.com>
2026-04-13 12:31:39 +0700

    feat: doi dinh dang Serial sang JSON (chuan xác)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,4 +1,4 @@
-int adcValues[3]; float nhietDo[3]; char chuoi[40];
+int adcValues[2]; float nhietDo[2]; char chuoi[40];
 
 void setup() {
   Serial.begin(9600);
@@ -6,21 +6,13 @@ void setup() {
 }
 
 void loop() {
-  // Kênh A0
   adcValues[0] = analogRead(A0);
   nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
   
-  // Kênh A1
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
   
-  // Kênh A2 (Vừa mới thêm)
-  adcValues[2] = analogRead(A2);
-  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
-  
-  // Xuất ra Serial
-// Sửa lại thành bản cũ để tạo mâu thuẫn
-sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
-Serial.print(chuoi);
+  sprintf(chuoi, "{\"A0\":%d, \"A1\":%d}\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
   delay(100);
 }
